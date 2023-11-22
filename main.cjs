773cf653979833d5e7c83bc482cdc8ad2012c7ba
Tomasz Lenarcik <apendua@gmail.com>
2023-11-22 15:40:59 +0100

    Fix types for CommonJS

--- main.cjs
+++ main.cjs
@@ -1,4 +1,4 @@
 const compare = require('./lib/compare.js');
 
 module.exports = compare.default;
-Object.assign(module.exports, compare);
+
---
+++ main.cjs.d.ts
@@ -0,0 +1,6 @@
+import compare from './lib/compare.js';
+
+// NOTE: This is to allow:
+//       const compare = require('compare');
+//       when someone is using a CommonJS module system.
+export = compare;
